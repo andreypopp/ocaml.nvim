b6fbd72badddbcaa68d4b0aa8f7a831ead3ce633
Andrey Popp <8mayday@gmail.com>
2024-08-13 01:14:16 +0400

    treat .mli as ocaml_interface filetype

--- lua/ocaml.lua
+++ lua/ocaml.lua
@@ -1,3 +1,8 @@
+function can_require(module_name)
+  local ok,_ = pcall(require, module_name)
+  return ok
+end
+
 local function get_ocamllsp()
   local cs = vim.lsp.get_active_clients()
   for _, c in ipairs(cs) do
@@ -10,6 +15,8 @@ local function with_ocamllsp(f)
   if c then f(c)
   else print('ERROR: ocamllsp is not running') end
 end
+
+--- switch between .ml and .mli
 
 local function switchIntfImpl()
   with_ocamllsp(function(client)
@@ -27,3 +34,22 @@ end
 
 vim.api.nvim_create_user_command(
   'OCamlSwitchIntfImpl', switchIntfImpl, {})
+
+--- treat .mli as separate ocaml_interface filetype
+
+vim.filetype.add { extension = { mli = 'ocaml_interface' } }
+
+
+if can_require 'lspconfig' then
+  local lspconfig = require 'lspconfig'
+
+  local ocamllsp_config = lspconfig.ocamllsp.document_config.default_config
+  table.insert(ocamllsp_config.filetypes, 'ocaml_interface')
+
+  local get_language_id = ocamllsp_config.get_language_id
+
+  function ocamllsp_config.get_language_id(bufnr, ftype)
+    if ftype == 'ocaml_interface' then return 'ocaml' 
+    else return get_language_id(bufnr, ftype) end
+  end
+end
